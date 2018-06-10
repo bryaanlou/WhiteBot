074441b674379f0fc2e6aefd1e07edfc053039f7
Bryan Lou <bryaan.lou@gmail.com>
2018-06-09 18:30:22 -0700

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const sql = require("sqlite");
-sql.open("./score.sqlite");
-
-sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
-    test = Math.floor(0.6 * Math.sqrt(row.points + 1));
-    console.log(test);
-});
